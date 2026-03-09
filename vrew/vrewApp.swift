bda91fccdca231d5879fd2fb258da8aa77422fa8
David Noreña <dach3r@gmail.com>
2026-03-09 10:07:56 -0500

    Initial Commit

---
+++ vrew/vrewApp.swift
@@ -0,0 +1,17 @@
+//
+//  vrewApp.swift
+//  vrew
+//
+//  Created by David Noreña on 9/03/26.
+//
+
+import SwiftUI
+
+@main
+struct vrewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
